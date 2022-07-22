84db17292c2adaa547c718aa775ac7cd4ead9f04
misut <pkr5207@gmail.com>
2022-07-23 01:07:36 +0900

    feat: add @xml/provider_paths

--- app/src/main/java/com/misut/errands/util/Intent.kt
+++ app/src/main/java/com/misut/errands/util/Intent.kt
@@ -7,8 +7,9 @@ import java.nio.file.Path
 
 
 fun Context.launchFileIntent(path: Path) {
-    val intent = Intent(Intent.ACTION_VIEW)
-    intent.data = FileProvider.getUriForFile(this, packageName, path.toFile())
-    intent.flags = Intent.FLAG_GRANT_READ_URI_PERMISSION
+    val intent = Intent(Intent.ACTION_VIEW).apply {
+        data = FileProvider.getUriForFile(this@launchFileIntent, packageName, path.toFile())
+        flags = Intent.FLAG_GRANT_READ_URI_PERMISSION
+    }
     startActivity(Intent.createChooser(intent, "Select Application"))
 }
